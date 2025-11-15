8c3eadd80afd3c241f2c19f3036e213e2c35affa
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 06:11:28 +0800

    Update Policies.jsx

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -1,12 +1,3 @@
 import React from 'react';
-
-function Policies() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>Policies Page</h2>
-      <p>Here you will manage all policies.</p>
-    </div>
-  );
-}
-
+function Policies() { return <div style={{padding:'20px'}}><h2>Policies</h2></div>; }
 export default Policies;
