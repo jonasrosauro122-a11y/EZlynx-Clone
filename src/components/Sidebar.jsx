2b5551b09c56ef49698ff0273c3eb547acc198fd
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 06:00:03 +0800

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,22 @@
-// src/components/Sidebar/Sidebar.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Sidebar.css';
 
-const Sidebar = () => {
+function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>Menu</h2>
-      <ul>
-        <li>Dashboard</li>
-        <li>Quote</li>
-        <li>Clients</li>
-        <li>Policies</li>
-      </ul>
+      <h2>EZLynx Clone</h2>
+      <nav>
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/clients">Clients</Link></li>
+          <li><Link to="/policies">Policies</Link></li>
+          <li><Link to="/home-quote">Home Quote</Link></li>
+          <li><Link to="/auto-quote">Auto Quote</Link></li>
+        </ul>
+      </nav>
     </div>
   );
-};
+}
 
 export default Sidebar;
