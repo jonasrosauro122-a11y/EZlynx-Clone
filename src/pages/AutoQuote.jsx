8284c93d482fa731e44383cb1355d8c38cb9688e
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 06:12:21 +0800

    Update AutoQuote.jsx

--- src/pages/AutoQuote.jsx
+++ src/pages/AutoQuote.jsx
@@ -1,13 +1,4 @@
 import React from 'react';
 import AutoQuoteForm from '../components/forms/AutoQuoteForm';
-
-function AutoQuote() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>Auto Quote</h2>
-      <AutoQuoteForm />
-    </div>
-  );
-}
-
+function AutoQuote() { return <div style={{padding:'20px'}}><h2>Auto Quote</h2><AutoQuoteForm/></div>; }
 export default AutoQuote;
