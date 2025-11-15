47398cc3790401b4f863195ff65dc2582111a9f2
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 05:34:17 +0800

    Update calculatePremium.js

--- src/utils/calculatePremium.js
+++ src/utils/calculatePremium.js
@@ -1,1 +1,9 @@
+// src/utils/calculatePremium.js
+export const calculatePremium = (data) => {
+  // Example logic
+  let premium = 500;
+  if (data.homeValue) premium += data.homeValue * 0.002;
+  if (data.autoValue) premium += data.autoValue * 0.01;
+  return premium.toFixed(2);
+};
 
