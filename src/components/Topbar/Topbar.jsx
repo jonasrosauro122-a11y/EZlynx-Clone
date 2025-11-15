593264dc007f38b5c66dff36fac1f493bf586e8d
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 05:31:45 +0800

    Update Topbar.jsx

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,1 +1,14 @@
+// src/components/Topbar/Topbar.jsx
+import React from 'react';
+import './Topbar.css';
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <h2>EZLynx Clone</h2>
+    </div>
+  );
+};
+
+export default Topbar;
 
