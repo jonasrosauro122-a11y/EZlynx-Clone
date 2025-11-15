a581c8e99934ab227cd3daca785279d7ef146b8f
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 04:46:22 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,33 @@
+import React from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
+import Topbar from './components/Topbar/Topbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Policies from './pages/Policies';
+import Claims from './pages/Claims';
+import Campaigns from './pages/Campaigns';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Sidebar />
+        <div className="main-content">
+          <Topbar />
+          <div className="page-content">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/policies" element={<Policies />} />
+              <Route path="/claims" element={<Claims />} />
+              <Route path="/campaigns" element={<Campaigns />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
