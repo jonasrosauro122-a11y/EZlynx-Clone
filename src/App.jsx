d7ed9250405add874151a299c730a0307de7306f
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 05:58:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Topbar from './components/Topbar/Topbar';
-import Sidebar from './components/Sidebar/Sidebar';
 import Dashboard from './pages/Dashboard';
-import Quote from './pages/Quote';
 import Clients from './pages/Clients';
 import Policies from './pages/Policies';
 import HomeQuote from './pages/HomeQuote';
 import AutoQuote from './pages/AutoQuote';
+import Sidebar from './components/Sidebar/Sidebar';
+import Topbar from './components/Topbar/Topbar';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div style={{ display: 'flex' }}>
+      <div className="app">
         <Sidebar />
-        <div style={{ flex: 1 }}>
+        <div className="main-content">
           <Topbar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/quote" element={<Quote />} />
             <Route path="/clients" element={<Clients />} />
             <Route path="/policies" element={<Policies />} />
             <Route path="/home-quote" element={<HomeQuote />} />
@@ -28,6 +26,6 @@ const App = () => {
       </div>
     </Router>
   );
-};
+}
 
 export default App;
