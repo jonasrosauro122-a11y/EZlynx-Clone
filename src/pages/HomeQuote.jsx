056c12ec2e42a2ec6199926b5ee9c7023608658a
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 06:11:42 +0800

    Update HomeQuote.jsx

--- src/pages/HomeQuote.jsx
+++ src/pages/HomeQuote.jsx
@@ -1,13 +1,4 @@
 import React from 'react';
 import HomeQuoteForm from '../components/forms/HomeQuoteForm';
-
-function HomeQuote() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>Home Quote</h2>
-      <HomeQuoteForm />
-    </div>
-  );
-}
-
+function HomeQuote() { return <div style={{padding:'20px'}}><h2>Home Quote</h2><HomeQuoteForm/></div>; }
 export default HomeQuote;
