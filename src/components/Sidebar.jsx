481aa3bc8065e1283498caf4321acaf169c962ee
jonasrosauro122-a11y <jonasrosauro122@gmail.com>
2025-11-16 05:31:09 +0800

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,19 @@
+// src/components/Sidebar/Sidebar.jsx
+import React from 'react';
+import './Sidebar.css';
 
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Menu</h2>
+      <ul>
+        <li>Dashboard</li>
+        <li>Quote</li>
+        <li>Clients</li>
+        <li>Policies</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
